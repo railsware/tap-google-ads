railsware/tap-google-ads

commit d880663a6cdc69a1240cd0ada9320eb2011d7767

Refresh data prior the latest bookmark depending on the conversion window value

tap_google_ads/streams.py
@@ -508,16 +508,19 @@ class BaseStream:  # pylint: disable=too-many-instance-attributes
         return record
 
 
-def get_query_date(start_date, bookmark, conversion_window_date):
+def get_query_date(start_date, bookmark, conversion_window):
     """Return a date within the conversion window and after start date
 
-    All inputs are datetime strings.
+    start_date and bookmark are datetime strings.
+    conversion_window is timedelta.
     NOTE: `bookmark` may be None"""
     if not bookmark:
         return singer.utils.strptime_to_utc(start_date)
     else:
-        query_date = min(bookmark, max(start_date, conversion_window_date))
-        return singer.utils.strptime_to_utc(query_date)
+        return max(
+            singer.utils.strptime_to_utc(bookmark) - conversion_window,
+            singer.utils.strptime_to_utc(start_date)
+        )
 
 
 class UserInterestStream(BaseStream):
@@ -716,7 +719,6 @@ class ReportStream(BaseStream):
         conversion_window = timedelta(
             days=get_conversion_window(config)
         )
-        conversion_window_date = utils.now().replace(hour=0, minute=0, second=0, microsecond=0) - conversion_window
 
         bookmark_object = singer.get_bookmark(state, stream["tap_stream_id"], customer["customerId"], default={})
 
@@ -725,7 +727,7 @@ class ReportStream(BaseStream):
         query_date = get_query_date(
             start_date=config["start_date"],
             bookmark=bookmark_value,
-            conversion_window_date=singer.utils.strftime(conversion_window_date)
+            conversion_window=conversion_window,
         )
 
         end_date = config.get("end_date")

commit 36486d24aa95543de2275e7792d38d33fcfab8c0

[CPL-16235] Fetch data by specified date columns only
Commits 2597485a4f1490992ee8dfeaecb419005d3c3de1 and
d880663a6cdc69a1240cd0ada9320eb2011d7767 accidentally
reverted to original tap-google-ads behaviour in which
tap fetches all reports day-by-day ONLY no matter
whether `date` column is requested in the config or not.

The tap must fetch data by months if `month` column
specified but `date` column is not.

The tap must fetch data by days if both `month` and
`date` columns are specified.

tap_google_ads/streams.py
@@ -726,7 +726,7 @@ class ReportStream(BaseStream):
             # Add inclusion metadata
             if self.behavior[report_field]:
                 inclusion = "available"
-                if transformed_field_name in ({"date"} | self.automatic_keys):
+                if transformed_field_name in self.automatic_keys:
                     inclusion = "automatic"
             else:
                 inclusion = "unsupported"