1ae5cc6ecf083591670e08038dbea557629069b7
Aikol Koikelov <aikol.koikelov@railsware.com>
2025-02-04 15:36:35 +0600

    [CPL-18749]: Added `Asset group performance` report

--- tap_google_ads/streams.py
+++ tap_google_ads/streams.py
@@ -1063,6 +1063,13 @@ def initialize_reports(resource_schema):
                 "ad_group_id",
              },
         ),
+        "asset_performance_report": ReportStream(
+            report_definitions.ASSET_GROUP_PERFORMANCE_REPORT_FIELDS,
+            ["asset_group"],
+            resource_schema,
+            ["_sdc_record_hash"],
+            {"asset_group_id"},
+        ),
         "campaign_performance_report": ReportStream(
             report_definitions.CAMPAIGN_PERFORMANCE_REPORT_FIELDS,
             ["campaign"],
