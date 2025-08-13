223fcdc06e56eb1396b32523dca5ab58eb4d2481
Maksym Ivannikov <maksym.ivannikov@railsware.com>
2025-08-13 16:31:46 +0300

    [CPL-21304] Upgrade to v21

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='tap-google-ads',
           'singer-python@git+https://github.com/railsware/singer-python/@565fcb685e6a636c3ad21e421a0662da47757573',
           'requests>=2.26.0',
           'backoff~=2.2.1',
-          'google-ads==27.0.0',
+          'google-ads==28.0.0',
           'protobuf==4.25.3',
           # Necessary to handle gRPC exceptions properly, documented
           # in an issue here: https://github.com/googleapis/python-api-core/issues/301
